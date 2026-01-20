85d6d542e009e6e1bc47dfaa549a59f8f6ddd7fd
kantor-rolland <rollandroli@yahoo.com>
2026-01-20 15:46:00 +0200

    Get database information: user and pass

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import os
 
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from models.article_comment_onetomany import Article, Comment
 from sqlalchemy.orm import sessionmaker
 
 from models.article_comment_onetomany import Base
 
-#load_dotenv()
+load_dotenv()
 
 # Read DB_USER and DB_PASS from environment variables
-#DB_USER = os.getenv("DB_USER", "postgres")
-#DB_PASS = os.getenv("DB_PASS", "postgres")
-#print(f"DB_USER: {DB_USER}, DB_PASS: {DB_PASS}")
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASS = os.getenv("DB_PASS", "postgres")
+print(f"DB_USER: {DB_USER}, DB_PASS: {DB_PASS}")
 
 
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./test.db'
